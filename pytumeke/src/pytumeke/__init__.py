293d5ffd9e72d1a5c5cef5cc6caceefd34e81b94
Diwakar Ganesan <diwakar.ganesan@gmail.com>
2021-05-18 16:04:15 -0700

    change loc

--- pytumeke/src/pytumeke/__init__.py
+++ pytumeke/src/pytumeke/__init__.py
@@ -2,14 +2,14 @@ import json
 
 __version__ = "1.0.1"
 
-f = open('../../config/rula.json')
+f = open("config/rula.json")
 rula_config = json.load(f)
 f.close()
 
-f = open('../../config/reba.json')
+f = open("config/reba.json")
 reba_config = json.load(f)
 f.close()
 
-f = open('../../config/niosh.json')
+f = open("config/niosh.json")
 niosh_config = json.load(f)
 f.close()
