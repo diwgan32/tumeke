87e96dca27d44f0fb198b9df2a19a272db09d80e
Diwakar Ganesan <diwakar.ganesan@gmail.com>
2021-05-18 20:28:14 -0700

    increment version

--- pytumeke/src/pytumeke/__init__.py
+++ pytumeke/src/pytumeke/__init__.py
@@ -2,7 +2,7 @@ import json
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 f = open(f"{dir_path}/config/rula.json")
 rula_config = json.load(f)
