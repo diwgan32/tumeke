99aba38dadeb4ed28ddb2e641d988e2415280684
Diwakar Ganesan <diwakar.ganesan@gmail.com>
2021-05-18 17:08:07 -0700

    change init

--- pytumeke/src/pytumeke/__init__.py
+++ pytumeke/src/pytumeke/__init__.py
@@ -4,14 +4,14 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 __version__ = "1.0.1"
 
-f = open("config/rula.json")
+f = open(f"{dir_path}/config/rula.json")
 rula_config = json.load(f)
 f.close()
 
-f = open("config/reba.json")
+f = open(f"{dir_path}/config/rula.json")
 reba_config = json.load(f)
 f.close()
 
-f = open("config/niosh.json")
+f = open(f"{dir_path}/config/rula.json")
 niosh_config = json.load(f)
 f.close()
