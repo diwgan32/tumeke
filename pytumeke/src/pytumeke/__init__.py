004b3fc1351b140e1171e006166f3985539bc1af
Diwakar Ganesan <diwakar.ganesan@gmail.com>
2021-05-18 17:22:03 -0700

    finalizing imports

--- pytumeke/src/pytumeke/__init__.py
+++ pytumeke/src/pytumeke/__init__.py
@@ -1,17 +1,17 @@
 import json
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+
 __version__ = "1.0.1"
 
 f = open(f"{dir_path}/config/rula.json")
 rula_config = json.load(f)
 f.close()
 
-f = open(f"{dir_path}/config/rula.json")
+f = open(f"{dir_path}/config/reba.json")
 reba_config = json.load(f)
 f.close()
 
-f = open(f"{dir_path}/config/rula.json")
+f = open(f"{dir_path}/config/niosh.json")
 niosh_config = json.load(f)
 f.close()
