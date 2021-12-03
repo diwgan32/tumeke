2f58b1fde4667e69f6a0cfe22f10f89383000150
znoland <znoland3@gmail.com>
2021-12-03 13:17:55 -0800

    Angle icons

--- tumekejs/compile.sh
+++ tumekejs/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 ./node_modules/.bin/babel src --out-dir lib
-yq eval -j ../config/rula.yaml &> config/rula.json
-yq eval -j ../config/reba.yaml &> config/reba.json
-yq eval -j ../config/niosh.yaml &> config/niosh.json
+yq eval -o=json ../config/rula.yaml &> config/rula.json
+yq eval -o=json ../config/reba.yaml &> config/reba.json
+yq eval -o=json ../config/niosh.yaml &> config/niosh.json
