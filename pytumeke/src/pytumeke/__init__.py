286940b021c38dc8f81e7b698f49a7fd83511590
Diwakar Ganesan <diwakar@tumeke.io>
2023-11-15 20:53:39 +0530

    libertymutual

--- pytumeke/src/pytumeke/__init__.py
+++ pytumeke/src/pytumeke/__init__.py
@@ -2,7 +2,7 @@ import json
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-__version__ = "1.0.19"
+__version__ = "1.0.20"
 
 f = open(f"{dir_path}/config/rula.json")
 rula_config = json.load(f)
@@ -18,6 +18,10 @@ f.close()
 
 f = open(f"{dir_path}/config/handstrain.json")
 handstrain_config = json.load(f)
+f.close()
+
+f = open(f"{dir_path}/config/libertymutual.json")
+lm_config = json.load(f)
 f.close()
 
 f = open(f"{dir_path}/config/skeleton.json")
