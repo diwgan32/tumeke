cbf56ea8e8beb381aae74f2ccccd8336560f64eb
Sergei Kazakov <kazakov.s.fl@gmail.com>
2025-08-28 01:07:55 +0400

    fix: Export mac.json

--- pytumeke/src/pytumeke/__init__.py
+++ pytumeke/src/pytumeke/__init__.py
@@ -2,7 +2,7 @@ import json
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-__version__ = "1.0.25"
+__version__ = "1.0.26"
 
 f = open(f"{dir_path}/config/rula.json")
 rula_config = json.load(f)
@@ -34,6 +34,10 @@ f.close()
 
 f = open(f"{dir_path}/config/rosa.json")
 rosa_config = json.load(f)
+f.close()
+
+f = open(f"{dir_path}/config/mac.json")
+mac_config = json.load(f)
 f.close()
 
 f = open(f"{dir_path}/config/strings.json")
